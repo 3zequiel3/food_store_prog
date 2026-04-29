55fcd7f80d3832097615b50a20d3e02d1b69561c
3zequiel3 <3zegonzalez@gmail.com>
2026-04-29 08:25:46 -0300

    Add route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { ClientLayout } from "./pages/client/ClientLayout";
-import { AdminLayout } from "./pages/admin/AdminLayout";
-import { CategoriasPage } from "./pages/admin/CategoriasPage";
-import { ProductosPage } from "./pages/admin/ProductosPage";
-import { ProductoDetallePage } from "./pages/admin/ProductoDetallePage";
-import { IngredientesPage } from "./pages/admin/IngredientesPage";
+import { BrowserRouter } from "react-router-dom";
+import AppRoute from "./routes/AppRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientLayout />} />
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<Navigate to="categorias" replace />} />
-          <Route path="categorias" element={<CategoriasPage />} />
-          <Route path="productos" element={<ProductosPage />} />
-          <Route path="productos/:id" element={<ProductoDetallePage />} />
-          <Route path="ingredientes" element={<IngredientesPage />} />
-        </Route>
-      </Routes>
+      <AppRoute />
     </BrowserRouter>
   );
 };
---
+++ frontend/src/routes/AppRoute.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ClientLayout } from "../pages/client/ClientLayout";
+import { AdminLayout } from "../pages/admin/AdminLayout";
+import { CategoriasPage } from "../pages/admin/CategoriasPage";
+import { ProductosPage } from "../pages/admin/ProductosPage";
+import { ProductoDetallePage } from "../pages/admin/ProductoDetallePage";
+import { IngredientesPage } from "../pages/admin/IngredientesPage";
+
+export default function AppRoute() {
+  return (
+    <Routes>
+        <Route path="/" element={<ClientLayout />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Navigate to="categorias" replace />} />
+          <Route path="categorias" element={<CategoriasPage />} />
+          <Route path="productos" element={<ProductosPage />} />
+          <Route path="productos/:id" element={<ProductoDetallePage />} />
+          <Route path="ingredientes" element={<IngredientesPage />} />
+        </Route>
+      </Routes>
+  )
+}
